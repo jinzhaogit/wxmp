d25302cab77afb2a473280a7e90a7698e04c2bca
jinzhao <2251366396@qq.com>
2018-10-27 21:05:09 +0800

    首页加新闻列表页初步开发(flex+rpx+bingTap+路由跳转+swiper组件+wx:for列表+module模块化数据+setDat)

---
+++ pages/data/newsdata.js
@@ -0,0 +1,33 @@
+// 新闻列表数据
+var newsData=[
+    {
+        newsid:1,
+        authoricon:'http://p1.music.126.net/BFVIggvKy42Da7qgz_0V4g==/109951163623534957.jpg?param=50y50',
+        authorname:'青梗姐姐',
+        authordate:'2018/10/27',
+        title:'水月不老，一世倾尘',
+        articleimg:'http://imge.kugou.com/soft/collection/480/20181025/20181025142303563124.jpg',
+        articletext:'你啊年少有为 而我年幼无知 可能因为这样 我们之间才不可能吧'
+      },
+      {
+        newsid:2,
+        authoricon:'http://p1.music.126.net/ToQg_54aMez6xVSQ64u5SA==/109951163120096532.jpg?param=50y50',
+        authorname:'图图弟弟',
+        authordate:'2018/10/27',
+        title:'天仙配',
+        articleimg:'http://imge.kugou.com/soft/collection/150/20181024/20181024183913892073.jpg',
+        articletext:'：我发现很多年少有为的人都是经历过很大的苦难挺过来才有今天的风光'
+      },
+      {
+        newsid:3,
+        authoricon:'http://p1.music.126.net/fJF6FfntBbK16X_SOr2OBA==/19117208672490654.jpg?param=50y50',
+        authorname:'听涛妹妹',
+        authordate:'2018/10/27',
+        title:'来年少有为',
+        articleimg:'http://imge.kugou.com/mvhdpic/400/20181025/20181025173642827459.jpg',
+        articletext:'总不能还没有努力就向生活妥协'
+      }
+]
+module.exports={
+    newsData:newsData
+}
